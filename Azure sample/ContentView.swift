7bec5020f57363229aa2c04dee592f15b9bf3993
Dharay <dharmistry@deloitte.com.com>
2020-05-04 10:21:29 +0530

    Initial Commit

---
+++ Azure sample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Azure sample
+//
+//  Created by Mistry, Dharay Paresh on 04/05/20.
+//  Copyright © 2020 Mistry, Dharay Paresh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! /n Azure sample")
+//        Text("Azure sample")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
